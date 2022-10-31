fe3c9c03b2c9c493b6e93f5bf2f1dd78980be7a5
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2022-10-31 20:58:53 +0200

    - Update Tailwind Config FIle. (#1)
    -  Add Header Link.
    - Add Footer Link.
    
    Co-authored-by: Md Mahbub Alam Khan <md.m.khan@tuni.fi>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,20 @@ import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
-    <div>
-      <p>Copyright &copy; 2022. </p>
-      <Link to="/">Home</Link> | <Link to="/about-us">About</Link> | <Link to="/terms">Terms</Link>
-    </div>
+    <>
+    <section className="mt-6 border-t-2 border-black-500">
+      <div className="container flex flex-col-reverse items-center justify-between mx-auto md:flex-row py-6 ">
+        <div className="text-left md:w-1/2">
+          <p>Copyright &copy; 2022. All Rights Reserved.</p>
+        </div>
+        <div className="space-x-6 mb-3 md:w-1/2 text-right">
+          <Link to="/" className="text-green-500">Home</Link> 
+          <Link to="/about-us" className="text-green-500">About</Link>
+          <Link to="/terms" className="text-green-500">Terms</Link>
+        </div>
+      </div>
+      </section>
+    </>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,25 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-    </div>
+    <>
+      <nav className="relative container mx-auto mt-6 mb-6">
+        
+        <div className="flex items-center justify-between">
+          
+          <div className="pt-2">
+              <Link to="/"><span className="text-2xl text-underline-none text-bold">Pocket Money</span></Link>
+          </div>
+         
+          <div className="space-x-6 text-right md:flex">
+            <Link to="/add-job" className="hover:text-darkGrayishBlue">Post A Task</Link>
+            <Link to="/search-job" className="hover:text-darkGrayishBlue">Search A Task</Link>
+          </div>
+
+         
+          {/* <Link to ="#" className="hidden p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight md:block">Get Started</Link> */}
+        </div>
+      </nav>
+    </>
   )
 }
 
