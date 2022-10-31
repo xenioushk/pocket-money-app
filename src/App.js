6540e2cb47d9ea452ef3406a84a0931c5d5c40b5
Md Mahbub Alam Khan <md.m.khan@tuni.fi>
2022-10-31 22:20:14 +0200

    Fixed header links.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 // import logo from "./logo.svg"
 // import Jobs from "./components/Jobs"
-// import Search from "./components/Search"
-// import AddJob from "./components/AddJob"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Home from "./components/Home"
 import AboutUs from "./components/AboutUs"
+import AddJob from "./components/AddJob"
+import Search from "./components/Search"
 import Terms from "./components/Terms"
 import SingleJob from "./components/SingleJob"
 
@@ -16,8 +16,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/add-job" element={<AddJob />} />
         <Route path="/job/:id" element={<SingleJob />} />
         <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/search-job" element={<Search />} />
         <Route path="/terms" element={<Terms />} />
       </Routes>
       <Footer />
