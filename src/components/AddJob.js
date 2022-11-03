5c4b853ec79b351ee1628ec28ae16ea9078be840
Md Mahbub Alam Khan <md.m.khan@tuni.fi>
2022-11-03 13:02:02 +0200

    Added Axios To The Form.

--- src/components/AddJob.js
+++ src/components/AddJob.js
@@ -1,25 +1,56 @@
 import React from "react"
+import FormHeading from "./form/FormHeading"
+import axios from "axios"
 import { useState } from "react"
 
 const AddJob = () => {
-  const [firstname, setFirstName] = useState("")
-  const [lastname, setLastName] = useState("")
-  const [contact, setContact] = useState("")
-  const [email, setEmail] = useState("")
-  const [address, setAddress] = useState("")
-  const [city, setCity] = useState("")
-  const [zipcode, setZipcode] = useState("")
+  const [firstname, setFirstName] = useState("Mahbub")
+  const [lastname, setLastName] = useState("Alam")
+  const [contact, setContact] = useState("098726364")
+  const [email, setEmail] = useState("hkhan.cse@gmail.com")
+  const [address, setAddress] = useState("Annalankatu,111 A12")
+  const [city, setCity] = useState("Tampere")
+  const [zipcode, setZipcode] = useState("33170")
 
-  const [taskTitle, setTaskTitle] = useState("")
-  const [taskDetails, settaskDetails] = useState("")
-  const [taskDuration, setTaskDuration] = useState("")
+  const [taskTitle, setTaskTitle] = useState("Baby Sitting")
+  const [taskDetails, settaskDetails] = useState("Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.")
+  const [taskDuration, setTaskDuration] = useState("1")
 
   //functions.
 
-  const handleSubmit = (e) => {
-    var isValid = false
+  const resetFields = () => {
+    setFirstName("")
+    setLastName("")
+    setContact("")
+    setEmail("")
+    setTaskTitle("")
+    settaskDetails("")
+    setTaskDuration("")
+  }
+
+  const newPost = {
+    userId: 1,
+    taskTitle: taskTitle,
+    taskDetails: taskDetails,
+    taskDuration: taskDuration,
+  }
+
+  const sendPostRequest = async () => {
+    try {
+      const resp = await axios.post("/wp-json/pmapi/v1/create", newPost)
+      // console.log(resp.data)
+      resetFields()
+    } catch (err) {
+      // Handle Error Here
+      console.error(err)
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    var isValid = true
 
     if (firstname.trim() === "") {
+      isValid = false
       console.log("Firstname Required.")
     }
 
@@ -31,9 +62,32 @@ const AddJob = () => {
       console.log("Contact Required.")
     }
 
-    if (isValid === true) {
-      alert("Submit Form")
+    if (taskTitle.trim() === "") {
+      isValid = false
+      console.log("Task Title Required.")
     }
+
+    if (taskDetails.trim() === "") {
+      isValid = false
+      console.log("Task Details Required.")
+    }
+
+    if (isValid === true) {
+      sendPostRequest()
+    }
+
+    // console.log(firstname)
+    // console.log(lastname)
+    // console.log(email)
+    // console.log(contact)
+    // console.log(address)
+    // console.log(city)
+    // console.log(zipcode)
+    // console.log(taskTitle)
+    // console.log(taskDetails)
+    // console.log(taskDuration)
+
+    // sendPostRequest()
 
     e.preventDefault()
   }
@@ -51,34 +105,34 @@ const AddJob = () => {
               <div className="overflow-hidden shadow sm:rounded-md">
                 <div className="bg-white px-4 py-5 sm:p-6">
                   <div className="grid grid-cols-8 gap-6">
-                    <h2 className="col-span-8 text-center text-2xl underline mb-3">Contact Details of Taks Poster</h2>
+                    <FormHeading title="Contact Details of Taks Poster" />
                     {/* First Name */}
                     <div className="col-span-4">
                       <label htmlFor="firstname" className="text-left block text-sm font-medium text-gray-700">
                         First name
                       </label>
-                      <input type="text" name="firstname" id="firstname" value={firstname} onChange={(e) => setFirstName(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <input type="text" name="firstname" id="firstname" value={firstname} onChange={(e) => setFirstName(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
                     {/* Last Name */}
                     <div className="col-span-4">
                       <label htmlFor="lastname" className="text-left block  text-sm font-medium text-gray-700">
                         Last name
                       </label>
-                      <input type="text" name="lastname" id="lastname" value={lastname} onChange={(e) => setLastName(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <input type="text" name="lastname" id="lastname" value={lastname} onChange={(e) => setLastName(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
                     {/* Email */}
                     <div className="col-span-4">
                       <label htmlFor="email" className="text-left block text-sm font-medium text-gray-700">
                         Email address
                       </label>
-                      <input type="text" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <input type="text" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
                     {/* Contact */}
                     <div className="col-span-4">
                       <label htmlFor="contact" className="text-left block text-sm font-medium text-gray-700">
                         Contact
                       </label>
-                      <input type="text" name="contact" id="contact" value={contact} onChange={(e) => setContact(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <input type="text" name="contact" id="contact" value={contact} onChange={(e) => setContact(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
                     {/* Street Address */}
                     <div className="col-span-8">
@@ -86,31 +140,30 @@ const AddJob = () => {
                         Address
                       </label>
 
-                      <textarea name="address" id="address" value={address} onChange={(e) => setAddress(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2"></textarea>
+                      <textarea name="address" id="address" value={address} onChange={(e) => setAddress(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2"></textarea>
                     </div>
                     {/* City */}
                     <div className="col-span-4">
                       <label htmlFor="city" className="text-left block text-sm font-medium text-gray-700">
                         City
                       </label>
-                      <input type="text" name="city" id="city" value={city} onChange={(e) => setCity(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <input type="text" name="city" id="city" value={city} onChange={(e) => setCity(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
                     {/* ZIP */}
                     <div className="col-span-4">
                       <label htmlFor="zipcode" className="text-left block text-sm font-medium text-gray-700">
                         ZIP / Postal code
                       </label>
-                      <input type="text" name="zipcode" id="zipcode" value={zipcode} onChange={(e) => setZipcode(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <input type="text" name="zipcode" id="zipcode" value={zipcode} onChange={(e) => setZipcode(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
 
-                    <h2 className="col-span-8 text-center text-2xl underline mb-3">Description of Task</h2>
-
+                    <FormHeading title="Description of Task" />
                     {/* Title of the Task */}
                     <div className="col-span-8">
                       <label htmlFor="taskTitle" className="text-left block text-sm font-medium text-gray-700">
                         Title of the task <small className="text-red">*</small>
                       </label>
-                      <textarea name="taskTitle" id="taskTitle" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2"></textarea>
+                      <input type="text" name="taskTitle" id="taskTitle" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
 
                     {/* Details of the Task */}
@@ -118,7 +171,7 @@ const AddJob = () => {
                       <label htmlFor="taskDetails" className="text-left block text-sm font-medium text-gray-700">
                         Details of the task <small className="text-red">*</small>
                       </label>
-                      <input type="text" name="taskDetails" id="taskDetails" value={taskDetails} onChange={(e) => settaskDetails(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <textarea name="taskDetails" id="taskDetails" value={taskDetails} onChange={(e) => settaskDetails(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2"></textarea>
                     </div>
 
                     {/* Duration of the Task */}
@@ -126,7 +179,7 @@ const AddJob = () => {
                       <label htmlFor="taskDuration" className="text-left block text-sm font-medium text-gray-700">
                         Duration of the task <small className="text-red">*</small>
                       </label>
-                      <input type="text" name="taskDuration" id="taskDuration" value={taskDuration} onChange={(e) => setTaskDuration(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <input type="text" name="taskDuration" id="taskDuration" value={taskDuration} onChange={(e) => setTaskDuration(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
                   </div>
                 </div>
