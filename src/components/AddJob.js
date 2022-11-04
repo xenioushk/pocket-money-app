4abe1bbd10915495710237c7f2ff6260977b7e13
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2022-11-04 10:06:06 +0200

    Pma 002 add task form (#5)
    * - added job post.
    
    * - Added Home Page Job Lists.
    
    * - Update add task form.
    
    * Added Axios To The Form.
    
    * - Update form fields.
    - Added Label component.
    - Update header and footer.
    
    * - Update read me file.
    
    Co-authored-by: Md Mahbub Alam Khan <md.m.khan@tuni.fi>

--- src/components/AddJob.js
+++ src/components/AddJob.js
@@ -1,7 +1,10 @@
-import React from "react"
-import { useState } from "react"
+import React, { useState } from "react"
+import FormHeading from "./form/FormHeading"
+import axios from "axios"
+import FormLabel from "./form/FormLabel"
 
 const AddJob = () => {
+  const [status, setStatus] = useState(0)
   const [firstname, setFirstName] = useState("")
   const [lastname, setLastName] = useState("")
   const [contact, setContact] = useState("")
@@ -14,12 +17,50 @@ const AddJob = () => {
   const [taskDetails, settaskDetails] = useState("")
   const [taskDuration, setTaskDuration] = useState("")
 
+  // useEffect(() => {
+  //   setStatus(1)
+  // }, [status])
+
   //functions.
 
-  const handleSubmit = (e) => {
-    var isValid = false
+  const resetFields = () => {
+    setFirstName("")
+    setLastName("")
+    setContact("")
+    setEmail("")
+    setCity("")
+    setZipcode("")
+    setAddress("")
+    setTaskTitle("")
+    settaskDetails("")
+    setTaskDuration("")
+  }
+
+  const newJob = {
+    userId: 1,
+    taskTitle: taskTitle,
+    taskDetails: taskDetails,
+    taskDuration: taskDuration,
+  }
+
+  const sendPostRequest = async () => {
+    try {
+      const resp = await axios.post("/wp-json/pmapi/v1/create", newJob)
+      // console.log(resp.data)
+      resetFields()
+    } catch (err) {
+      // Handle Error Here
+      console.error(err)
+    }
+  }
+  // Error Class.
+  // border-red-500
+
+  const handleSubmit = async (e) => {
+    var isValid = true
 
     if (firstname.trim() === "") {
+      isValid = false
       console.log("Firstname Required.")
     }
 
@@ -31,17 +72,46 @@ const AddJob = () => {
       console.log("Contact Required.")
     }
 
-    if (isValid === true) {
-      alert("Submit Form")
+    if (taskTitle.trim() === "") {
+      isValid = false
+      console.log("Task Title Required.")
     }
 
+    if (taskDetails.trim() === "") {
+      isValid = false
+      console.log("Task Details Required.")
+    }
+
+    if (isValid === true) {
+      sendPostRequest()
+    }
+
+    // console.log(firstname)
+    // console.log(lastname)
+    // console.log(email)
+    // console.log(contact)
+    // console.log(address)
+    // console.log(city)
+    // console.log(zipcode)
+    // console.log(taskTitle)
+    // console.log(taskDetails)
+    // console.log(taskDuration)
+
+    // sendPostRequest()
+
+    window.scrollTo({ top: 0, behavior: "smooth" })
+    setStatus(1)
+    setTimeout(() => {
+      setStatus(0)
+    }, 3000)
     e.preventDefault()
   }
 
   return (
     <>
       <div>
-        <div className="flex flex-wrap  justify-center mt-20">
+        <div className={`text-center bg-brightRed justify-center mx-auto w-1/3 p-3 text-white ${status ? "" : "hidden"}`}>Job created successfully!</div>
+        <div className="flex flex-wrap justify-center mt-20">
           <h1 className="text-4xl underline underline-offset-4">Post a Task</h1>
         </div>
 
@@ -51,82 +121,63 @@ const AddJob = () => {
               <div className="overflow-hidden shadow sm:rounded-md">
                 <div className="bg-white px-4 py-5 sm:p-6">
                   <div className="grid grid-cols-8 gap-6">
-                    <h2 className="col-span-8 text-center text-2xl underline mb-3">Contact Details of Taks Poster</h2>
+
+                    <FormHeading title="Contact Details of Taks Poster" />
                     {/* First Name */}
                     <div className="col-span-4">
-                      <label htmlFor="firstname" className="text-left block text-sm font-medium text-gray-700">
-                        First name
-                      </label>
-                      <input type="text" name="firstname" id="firstname" value={firstname} onChange={(e) => setFirstName(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <FormLabel id="firstname" title="First Name" />
+                      <input type="text" name="firstname" id="firstname" value={firstname} onChange={(e) => setFirstName(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
                     {/* Last Name */}
                     <div className="col-span-4">
-                      <label htmlFor="lastname" className="text-left block  text-sm font-medium text-gray-700">
-                        Last name
-                      </label>
-                      <input type="text" name="lastname" id="lastname" value={lastname} onChange={(e) => setLastName(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <FormLabel id="lastname" title="Last Name" required={false} />
+                      <input type="text" name="lastname" id="lastname" value={lastname} onChange={(e) => setLastName(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
                     {/* Email */}
                     <div className="col-span-4">
-                      <label htmlFor="email" className="text-left block text-sm font-medium text-gray-700">
-                        Email address
-                      </label>
-                      <input type="text" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <FormLabel id="email" title="Email" />
+                      <input type="text" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
                     {/* Contact */}
                     <div className="col-span-4">
-                      <label htmlFor="contact" className="text-left block text-sm font-medium text-gray-700">
-                        Contact
-                      </label>
-                      <input type="text" name="contact" id="contact" value={contact} onChange={(e) => setContact(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <FormLabel id="contact" title="Contact" />
+                      <input type="text" name="contact" id="contact" value={contact} onChange={(e) => setContact(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
                     {/* Street Address */}
                     <div className="col-span-8">
-                      <label htmlFor="address" className="text-left block text-sm font-medium text-gray-700">
-                        Address
-                      </label>
-
-                      <textarea name="address" id="address" value={address} onChange={(e) => setAddress(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2"></textarea>
+                      <FormLabel id="address" title="Address" />
+                      <textarea name="address" id="address" value={address} onChange={(e) => setAddress(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2"></textarea>
                     </div>
                     {/* City */}
                     <div className="col-span-4">
-                      <label htmlFor="city" className="text-left block text-sm font-medium text-gray-700">
-                        City
-                      </label>
-                      <input type="text" name="city" id="city" value={city} onChange={(e) => setCity(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <FormLabel id="city" title="City" />
+                      <input type="text" name="city" id="city" value={city} onChange={(e) => setCity(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
                     {/* ZIP */}
                     <div className="col-span-4">
-                      <label htmlFor="zipcode" className="text-left block text-sm font-medium text-gray-700">
-                        ZIP / Postal code
-                      </label>
-                      <input type="text" name="zipcode" id="zipcode" value={zipcode} onChange={(e) => setZipcode(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <FormLabel id="zipcode" title="Zip / Postal code" />
+                      <input type="text" name="zipcode" id="zipcode" value={zipcode} onChange={(e) => setZipcode(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
 
-                    <h2 className="col-span-8 text-center text-2xl underline mb-3">Description of Task</h2>
-
+                    <FormHeading title="Description of Task" />
                     {/* Title of the Task */}
                     <div className="col-span-8">
-                      <label htmlFor="taskTitle" className="text-left block text-sm font-medium text-gray-700">
-                        Title of the task <small className="text-red">*</small>
-                      </label>
-                      <textarea name="taskTitle" id="taskTitle" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2"></textarea>
+                      <FormLabel id="taskTitle" title="Title of the task" />
+                      <input type="text" name="taskTitle" id="taskTitle" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
                     </div>
 
                     {/* Details of the Task */}
                     <div className="col-span-8">
-                      <label htmlFor="taskDetails" className="text-left block text-sm font-medium text-gray-700">
-                        Details of the task <small className="text-red">*</small>
-                      </label>
-                      <input type="text" name="taskDetails" id="taskDetails" value={taskDetails} onChange={(e) => settaskDetails(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <FormLabel id="taskDetails" title=" Details of the task" />
+                      <textarea name="taskDetails" id="taskDetails" value={taskDetails} onChange={(e) => settaskDetails(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2"></textarea>
+
                     </div>
 
                     {/* Duration of the Task */}
                     <div className="col-span-8">
-                      <label htmlFor="taskDuration" className="text-left block text-sm font-medium text-gray-700">
-                        Duration of the task <small className="text-red">*</small>
-                      </label>
-                      <input type="text" name="taskDuration" id="taskDuration" value={taskDuration} onChange={(e) => setTaskDuration(e.target.value)} className="mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+                      <FormLabel id="taskDuration" title="Duration of the task" />
+                      <input type="text" name="taskDuration" id="taskDuration" value={taskDuration} onChange={(e) => setTaskDuration(e.target.value)} className="p-2 mt-2 block w-full rounded border-2 border-gray-500 focus:border-2" />
+
                     </div>
                   </div>
                 </div>
