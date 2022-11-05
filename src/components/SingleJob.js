8ecbd447132cf37037721b85e5f51b6c7739bd5b
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2022-11-05 22:19:42 +0200

    Pm 013 search request to backend (#7)
    * - Added search box.
    - Added Load more button (PM-015)
    
    * - Added single page.
    - Improve search list.
    
    Co-authored-by: Md Mahbub Alam Khan <md.m.khan@tuni.fi>

--- src/components/SingleJob.js
+++ src/components/SingleJob.js
@@ -1,7 +1,48 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import JobItem from "./JobItem"
+import axios from "axios"
 
-const SingleJob = (props) => {
-  return <div>ajdfljasldjflajsdlfjaldsjfl{props.id}</div>
+const SingleJob = () => {
+  // get ID from url
+  const params = useParams()
+
+  // console.log(params.id) // {userId: '4200'}
+
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [singleJob, setSingleJob] = useState([])
+
+  useEffect(() => {
+    // GET request using axios inside useEffect React hook
+
+    const fetchData = () => {
+      axios
+        .get(`/wp-json/pmapi/v1/jobs?p_id=${params.id}`)
+        .then((res) => {
+          setIsLoaded(true)
+          setSingleJob((prev) => prev.concat(res.data.job_data))
+        })
+        .catch((err) => console.log(err))
+    }
+
+    fetchData()
+  }, [])
+
+  return (
+    <>
+      {isLoaded ? (
+        <div className="container mx-auto items-center">
+          <div className="grid grid-cols-1 gap-y-4">
+            {singleJob.map((job, index) => (
+              <JobItem key={index} job={job} single={true} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="text-center">Loading...</div>
+      )}
+    </>
+  )
 }
 
 export default SingleJob
