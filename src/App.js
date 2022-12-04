b5778a838cf76d11dbd3b957ab134f464c975e8b
unknown <mahmud.hossain@tuni.fi>
2022-12-04 21:02:48 +0200

    Added title on every single job page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Header from "./components/shared/Header"
-import Footer from "./components/shared/Footer"
-import Home from "./components/pages/Home"
-import AboutUs from "./components/pages/AboutUs"
-import AddJob from "./components/form/AddJob"
-import SearchBox from "./components/search/SearchBox"
-import Terms from "./components/pages/Terms"
-import Category from "./components/pages/Category"
-import SingleJob from "./components/pages/SingleJob"
-import EditJob from "./components/pages/EditJob"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/shared/Header";
+import Footer from "./components/shared/Footer";
+import Home from "./components/pages/Home";
+import AboutUs from "./components/pages/AboutUs";
+import AddJob from "./components/form/AddJob";
+import SearchBox from "./components/search/SearchBox";
+import Terms from "./components/pages/Terms";
+import Category from "./components/pages/Category";
+import SingleJob from "./components/pages/SingleJob";
+import EditJob from "./components/pages/EditJob";
 
 function App() {
   return (
     <BrowserRouter>
+      ``
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -26,7 +27,7 @@ function App() {
       </Routes>
       <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,18 +1,18 @@
-import { useEffect } from "react"
-import SearchBox from "../search/SearchBox"
-import Jobs from "../jobs/Jobs"
-import Page from "../pages/Page"
+import { useEffect } from "react";
+import SearchBox from "../search/SearchBox";
+import Jobs from "../jobs/Jobs";
+import Page from "../pages/Page";
 
 const Home = () => {
   useEffect(() => {
-    document.title = "Home"
-  }, [])
+    document.title = "Home";
+  }, []);
   return (
-    <Page title="Home">
+    <Page>
       <SearchBox />
       <Jobs />
     </Page>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
