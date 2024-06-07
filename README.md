b7375065ebc677b7d28df42092567320a7b20473
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2024-06-07 21:10:33 +0600

    Updated .env file to the project root directory. (#40)
    Updated all the static code into the code and safely moved all the dynamic variables into the .env file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Pocket Money React Application
 
 You can check demo in here: [https://pocket-money-23.netlify.app/](https://pocket-money-23.netlify.app/)
+
+# Instructions
+
+1. Clone the project to your local machine.
+2. Copy the sample_env file and rename it to .env
+3. Add all the required information in there.
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import axios from "axios"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
-axios.defaults.baseURL = process.env.REACT_APP_BACKENDURL || "https://pmapi.bluewindlab.com"
+axios.defaults.baseURL = process.env.REACT_APP_PM_API_URL || "https://pmapi.bluewindlab.com"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<App />)
