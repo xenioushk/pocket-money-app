061931c80551960f554b9ae097a64f30ebc52252
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-12-30 19:48:28 +0200

    fix: api base url path.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import axios from "axios"
 import "./index.css"
 import App from "./App"
 
-axios.defaults.baseURL = import.meta.env.VITE_PM_API_URL || "https://pmapi.bluewindlab.com"
+axios.defaults.baseURL = import.meta.env.VITE_PM_API_URL || "https://pocket-money-app-backend.onrender.com"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(<App />)
